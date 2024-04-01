55d6069cebc81fad358394161d9dcf1460a5752e
Zehra <95143820+aydinfz@users.noreply.github.com>
2024-04-02 00:53:25 +0300

    style: navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,5 @@
 "use client"
-
-import {useState} from "react"
 import Link from 'next/link'
-
 
 export default function Navbar() {
     return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'logo-color': '#9100D9',
-        'custom-orange' : '#FF9248',
-        'card-orange' : '#FF924880',
-        'text-color': '#3A3636'
+        "primary-light": "#FFFE88",
+        "primary-dark": "#000000",
+        "logo-color": "#9100D9",
+        "custom-orange": "#FF9248",
+        "card-orange": "#FF924880",
+        "text-color": "#3A3636",
+      },
+      backgroundImage: {
+        world: "url(./assets/world.png)",
+        pins: "url(./assets/pins.png)",
+        passport: "url(./assets/passport.png)",
+        notebook: "url(./assets/notebook.png)",
+        "leather-notebook": "url(./assets/leather-notebook.png)",
+      },
+      spacing: {
+        30: "30%",
+        40: "40%",
+        50: "50%",
+      },
     },
-     backgroundImage: {
-      'world' : "url(./assets/world.png)",
-      'pins' : "url(./assets/pins.png)",
-      'passport' : "url(./assets/passport.png)",
-      'notebook' : "url(./assets/notebook.png)",
-      'leather-notebook' : "url(./assets/leather-notebook.png)"
-     },
-     spacing: {
-      '30': '30%',
-      '40': '40%',
-      '50': '50%',
-    },
+    plugins: [],
   },
-  plugins: [],
- }
-}
+};
