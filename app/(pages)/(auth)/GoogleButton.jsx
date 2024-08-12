14131b632e4cce7c558e6cc417fff94234600300
Zehra <95143820+zehraworks@users.noreply.github.com>
2024-08-12 15:58:17 +0300

    chore: folder structure

--- app/(pages)/(auth)/GoogleButton.jsx
+++ app/(pages)/(auth)/GoogleButton.jsx
@@ -2,16 +2,16 @@
 
 import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
-import { BsGithub } from "react-icons/bs";
+import { BsGoogle } from "react-icons/bs";
 
-export default function GoogleSignInButton() {
+export default function GoogleButton() {
   return (
     <Button
       className="w-full flex bg-transparent text-primary-foreground rounded-sm border-[1px]  border-primary-foreground "
-      onClick={() => signIn("github", { callbackUrl: "/" })}
+      onClick={() => signIn("google", { callbackUrl: "/" })}
     >
-      <BsGithub className="h-5 w-5 mr-4 text-primary-foreground" />
-      <p>Continue with Github</p>
+      <BsGoogle className="h-5 w-5 mr-3 text-primary-foreground" />
+      <p>Continue with Google</p>
     </Button>
   );
 }
--- app/(pages)/(auth)/signup/page.jsx
+++ app/(pages)/(auth)/signup/page.jsx
@@ -1,10 +1,10 @@
 "use client";
-import GoogleSignInButton from "../../components/auth/GoogleSignInButton";
-import GithubSignInButton from "../../components/auth/GithubSignInButton";
-import SignInForm from "./form";
+import GoogleButton from "../GoogleButton";
+import GithubButton from "../GithubButton";
 import Image from "next/image";
+import SignUpForm from "./form";
 
-export default function SignIn() {
+export default function Signup() {
   return (
     <div className="flex bg-background full-height">
       <div className="relative w-60">
@@ -15,20 +15,25 @@ export default function SignIn() {
           alt="world tour"
         />
       </div>
-      <div className="flex flex-col items-center justify-center text-center w-[440px] h-auto my-4 bg-primary text-primary-foreground px-16 py-16 mx-auto space-y-5 rounded-bl-sm rounded-tr-sm rounded-br-4xl rounded-tl-4xl border-[1.5px] border-primary-foreground">
+      <div className="flex flex-col items-center text-center w-[440px] h-min-[700px] bg-primary text-primary-foreground px-16 py-8 my-8 mx-auto space-y-3 rounded-bl-sm rounded-tr-sm rounded-br-4xl rounded-tl-4xl border-[1.5px] border-primary-foreground">
         <h1 className="w-full font-bold text-3xl">Welcome to Travelog</h1>
-        <p className="font-light text-xl">We saved you a seat</p>
-        <SignInForm /> 
+        <div className="space-y-1">
+          <p className="font-light text-xl">We saved you a seat</p>
+          <p className="font-extralight text-xs">
+            Create a free account to get started.
+          </p>
+        </div>
+        <SignUpForm />
         <div className=" flex items-center w-full">
           <div className="flex-grow border-t border-primary-foreground"></div>
           <span className="mx-2 text-primary-foreground">or</span>
           <div className="flex-grow border-t border-primary-foreground"></div>
         </div>
         <div className="w-full space-y-3">
-          <GoogleSignInButton />
-          <GithubSignInButton />
-          <p className="text-sm underline tracking-wide underline-offset-2">
-            Create a new Travelog account.
+          <GoogleButton />
+          <GithubButton />
+          <p className="text-xs underline tracking-wide underline-offset-2">
+            Do you have an account? Login here
           </p>
         </div>
       </div>
