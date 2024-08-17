3ad3b5073b6ab317d05911a9ee0072560859a11a
Zehra <95143820+zehraworks@users.noreply.github.com>
2024-08-17 15:21:43 +0300

    config: next

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
   env: {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    MONGODB_URL: process.env.MONGODB_URL,
   },
   images: {
     domains: ["lh3.googleusercontent.com"],
