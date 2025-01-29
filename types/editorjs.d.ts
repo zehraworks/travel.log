d67608a1c512c0aa195b582b8c2c84d4e48b11cf
Zehra <95143820+zehraworks@users.noreply.github.com>
2025-01-29 21:00:21 +0300

    fix: declare types for editorjs

---
+++ types/editorjs.d.ts
@@ -0,0 +1,6 @@
+declare module '@editorjs/checklist';
+declare module '@editorjs/marker';
+declare module '@editorjs/list';
+declare module '@editorjs/embed';
+declare module 'editorjs-text-color-plugin';
+declare module 'editorjs-text-alignment-blocktune';
